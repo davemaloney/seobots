b9bab79291bfa3333c2c9b2e36fd61df672ea468
Dave Maloney <david.s.maloney@gmail.com>
2017-08-03 22:43:49 -0400

    Move shared start_requests def outside the spiders. DRY

--- seobots/spiders/seo_spider.py
+++ seobots/spiders/seo_spider.py
@@ -1,80 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider
 from scrapy.selector import Selector
 from seobots.items import Page
-
-import urllib2
+from start_requests import start_requests
 
 class SEOSpider(CrawlSpider):
-    # Setting up the CrawlSpider
     name = "seo_spider"
 
-    # urllib2 is sync however we're only using these methods once to initialize the crawler.
-    @staticmethod
-    def remote_file_to_array(url):
-        # read, split, filter, return all non-empty lines
-        return filter(None, urllib2.urlopen(url).read().splitlines())
-
-    @staticmethod
-    def sitemap_to_array(url):
-        results = []
-        body = urllib2.urlopen(url).read()
-        sitemap = Sitemap(body)
-        for item in sitemap:
-            results.append(item['loc'])
-        return results
-
-    # __init__ is called to get the spider name so avoid doing any extra work
-    # in init such as downloading files.
-    #
-    # args are automatically made available to the spider.
-
-    def start_requests(self):
-        # update rules
-        # load target domain and then use it once to define the rules
-        # target domain is a string value.
-        allowed_domains = self.domain
-        print 'Target domain: ', allowed_domains
-
-        # If a link matches multiple rules, the first rule wins.
-        self.rules = (
-            # If a link is within the target domain, follow it.
-            Rule(LinkExtractor(allow_domains=[allowed_domains], unique=True),
-                 callback='parse_item',
-                 process_links='clean_links',
-                 follow=True),
-            # Crawl external links and don't follow them
-            # Rule(LinkExtractor(unique=True),
-            #     callback='parse_item',
-            #     process_links='clean_links',
-            #     follow=False),
-        )
-        self._compile_rules()
-
-        # now deal with requests
-        start_urls = []
-        if self.startpage.endswith('.xml'):
-            print 'Sitemap detected!'
-            start_urls = self.sitemap_to_array(self.startpage)
-        elif self.startpage.endswith('.txt'):
-            print 'Remote url list detected!'
-            start_urls = self.remote_file_to_array(self.startpage)
-        else: # single url
-            start_urls = [self.startpage]
-        print 'Start url count: ', len(start_urls)
-        first_url = start_urls[0]
-        print 'First url: ', first_url
-
-        # must set dont_filter on the start_urls requests otherwise
-        # they will not be recorded in the items output because it'll
-        # be considered a duplicate url.
-        # see https://github.com/scrapy/scrapy/blob/5daa14770b23da250ccfd4329899a1c3f669b1f3/scrapy/spider.py#L65
-        for url in start_urls:
-            # pass array of dictionaries to set cookies.
-            # http://doc.scrapy.org/en/latest/topics/request-response.html#topics-request-response
-            yield scrapy.Request(url, dont_filter=True)
+    start_requests = start_requests
 
     # Parsing every item (page) the spider will crawl
     def parse_item(self, response):
@@ -83,10 +17,10 @@ class SEOSpider(CrawlSpider):
         item['address'] = response.url
         item['content'] = response.headers.get('Content-Type')
         item['status_code'] = response.status
-        # item['status'] = 
-        # item['response_time'] = 
-        # item['hash'] = 
-        # item['last_modified'] = 
+        # item['status'] =
+        # item['response_time'] =
+        # item['hash'] =
+        # item['last_modified'] =
         item['title'] = Selector(response).xpath('//title/text()').extract()
         item['title_length'] = len( ''.join( item['title'] ) )
         # item['title pixel width'] = 
@@ -105,7 +39,7 @@ class SEOSpider(CrawlSpider):
         item['h1'] = Selector(response).xpath('//h1/text()').extract_first()
         item['h1_length'] = len( ''.join( item['h1'] ) )
         item['h1_count'] = len( Selector(response).xpath('//h1/text()').extract() )
-        item['h2'] = Selector(response).xpath('//h2/text()')[0].extract()
+        item['h2'] = Selector(response).xpath('//h2/text()').extract_first()
         item['h2_length'] = len( ''.join( item['h2'] ) )
         item['h2_2'] = Selector(response).xpath('//h2/text()')[1].extract()
         item['h2_2_length'] = len( ''.join( item['h2_2'] ) )
